efb7b47e8e6c0d31860c1b494d6d40d3ad52784a
Rob Campbell <git@raacampbell.com>
2015-09-14 12:38:23 +0200

    add sparse points transform to transformix examples.

--- MelastiX_examples/transformix/RUN_ALL.m
+++ MelastiX_examples/transformix/RUN_ALL.m
@@ -7,5 +7,8 @@ pause
 
 %Note: the parameter files are slightly different here so you don't get quite the same image back
 applyLenaTransform2Dog_withParams
+disp('PRESS RETURN')
+pause
 
 
+transformSparsePoints_forward
